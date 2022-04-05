693ad74e3c7bf8915b912a9981866664bf2d3877
Dmitrij Tolstov <tolstov.dmitrij42@gmail.com>
2022-04-05 18:59:11 +0400

    First Project

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+    $('.design__slider').slick({
+        dots: false,
+        slidesToShow: 4,
+        variableWidth: true,
+        prevArrow: '<img src="images/arrow-left.svg" alt="" class="arrow arrow-left">',
+        nextArrow: '<img src="images/arrow-right.svg" alt="" class="arrow arrow-right">',
+        responsive:[{
+            breakpoint: 361,
+            settings:{
+            slidesToShow: 1,
+            variableWidth: false,
+            }
+        }]
+    });
+});
